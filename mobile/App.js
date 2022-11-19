0c7bc829b1d048c8f8156bb516ab5d9acc87f9ba
ge85qug <ge85qug@tum.de>
2022-11-19 18:24:21 +0100

    Add react native elements

--- mobile/App.js
+++ mobile/App.js
@@ -22,7 +22,7 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        {!phone_number ? (
+        {true ? (
           <>
           <Stack.Screen
             name="Signup"
--- mobile/src/Signup.js
+++ mobile/src/Signup.js
@@ -2,6 +2,7 @@ import React, {useEffect, useState} from "react";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 import { savePhoneNumber } from "../storage";
+import { Input, Icon } from '@rneui/themed';
 
 export default function Signup({ navigation }) {
   const [value, onChangeText] = useState("123456");
@@ -10,6 +11,12 @@ export default function Signup({ navigation }) {
     await savePhoneNumber(value);
     navigation.navigate("Home");
   };
+
+  const input = React.createRef();
+
+  useEffect(() => {
+    input.current.focus()
+  }, [])
   return (
     <View style={styles.container}>
       <Text>Register by signing up through with your phone number!</Text>
@@ -19,9 +26,13 @@ export default function Signup({ navigation }) {
         editable
         maxLength={40}
       />
+      <Input
+        ref={input}
+        placeholder="Phone Number" secureTextEntry={false}
+      />
       <Button
         onPress={submit}
-        title="Go to Home"
+        title="Signup"
         color="#841584"
         accessibilityLabel="Learn more about this purple button"
       />
@@ -39,3 +50,9 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
 });
+
+function focusTextInput() {
+  // Explicitly focus the text input using the raw DOM API
+  // Note: we're accessing "current" to get the DOM node
+  this.textInput.current.focus();
+}
